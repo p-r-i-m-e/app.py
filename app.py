6189bbb2985c5286ff8c60ed40b83eb98a2a8b25
EXTE&SION <86994680+p-r-i-m-e@users.noreply.github.com>
2024-05-31 21:32:40 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from bot import create_app
 
 # Create a Flask application
 app = Flask(__name__)
 
+# Initialize the bot application
+bot_token = "010824792:AAGX8uLjw1eN_d-TyxDHhXMTGlhtvgUADO4"  # Replace with your actual bot token
+telegram_bot_app = create_app(bot_token)
+
 # Define a route for the homepage
 @app.route('/')
 def hello_world():
-    return 'This bot is made by Ezikel '
+    return 'This Bot is made by Ezikel'
+
+# Define a route for Telegram webhook
+@app.route('/webhook', methods=['POST'])
+def webhook() -> str:
+    update = telegram_bot_app.update_queue.get_update(request.get_json(force=True))
+    telegram_bot_app.process_update(update)
+    return 'ok'
 
 # Run the application
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
